12a2e2650c06c80e8eb2049e7e656f71367c912c
DanikhoirR <zidane.khr08@gmail.com>
2024-08-18 11:59:06 +0700

    perbaikan dan penambahan

--- exercise-method/1.string_method.js
+++ exercise-method/1.string_method.js
@@ -1,21 +1,21 @@
-//* String.Prototype.at()
+// * String.Prototype.at()
 // const sentence = "baba babi babayo bagus basi";
 // let index = 7;
 // console.log(`index: ${index}, returns the char ${sentence.at(index)}`);
 // index = -7;
 // console.log(`index: ${index}, returns the char ${sentence.at(index)}`)
 
-//* String.prototype.charAt()
+// * String.prototype.charAt()
 // const anyString = "Pada suatu hari budi pergi ke anatah branatah";
 // console.log(`index 0 is ${anyString.charAt(0)}`);
 // console.log(`index 1 is ${anyString.charAt(1)}`);
 // console.log(`index 2 is ${anyString.charAt(2)}`);
 
-//* String.prototype.charCodeAt()
+// * String.prototype.charCodeAt()
 // const type = "Mencari kitab scui bersama Sun Go KUng";
 // let index = 6;
 // console.log(`code char: ${type.charCodeAt(index)} is equal to ${type.charAt()}`);
 
-//* String.prototype.codePointAt()
+// * String.prototype.codePointAt()
 // let icon = '☃★♲';
 // console.log(icon.codePointAt(2));
